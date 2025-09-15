579b3404faa7a61c92a441649fca8601aa2ef916
ronaldo07526 <r32315063@gmail.com>
2025-09-15 15:11:55 +0100

    Update config.js

--- config.js
+++ config.js
@@ -3,7 +3,7 @@ import settings from './settings.js';
 export default {
   // Bot configuration
   prefix: '.', // input your prfic here 
-  ownerNumber: '', //input your number here 
+  ownerNumber: '2348020827575', //input your number here 
   botName: '✦✦✦ 𝐇 𝐎 𝐑 𝐋 𝐀 𝐏 𝐎 𝐎 𝐊 𝐈 𝐄 ✦✦✦',
   ownerName: '𝓗𝓞𝓡𝓛𝓐𝓟𝓞𝓞𝓚𝓘𝓔', //optional 
   sessionId: 'HORLA-POOKIE-SESSION-ID', //sensitive name 
